5c6c70cd492a22b5578f33f975bbbeabc10d8e1b
simlythorn <thornsimly17406@gmail.com>
2023-06-11 11:37:51 +0700

    add space in isplayResult function

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -33,7 +33,7 @@ class Program
         }
         static void DisplayResult(string name, int square)
         {
-            Console.WriteLine($"{name}, the square of your number is{square}");
+            Console.WriteLine($"{name}, the square of your number is {square}");
         }
     }
 }
