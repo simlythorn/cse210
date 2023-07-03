812bd6265e5a54d290e324599cf5aff5ce11b6ec
simlythorn <thornsimly17406@gmail.com>
2023-07-03 23:23:28 +0700

    finished learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Circle circle1 = new Circle("black", 3.56);
+        shapes.Add(circle1);
+
+        Circle circle2 = new Circle("pink", 2.56);
+        shapes.Add(circle2);
+        
+        Rectangle rectangle = new Rectangle("gray", 1.34, 2.78);
+        shapes.Add(rectangle);
+
+        Square square = new Square("green", 1.34);
+        shapes.Add(square);
+        
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"{color} area is: {area}");
+        }
+
     }
 }
---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,19 @@
+using System;
+public abstract class Shape
+{
+    public string _color;
+    public Shape(string color)
+    {
+        _color = color;
+    }
+    public string GetColor()
+    {
+        return _color;
+    }
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+    public abstract double GetArea();
+  
+}
